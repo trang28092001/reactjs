75b30a9b771b615099585da4579017fc3a52112b
Trang <trang28092001@gmail.com>
2022-10-10 11:52:04 +0700

    Add event press enter.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  
+  const [task, setStask] = useState();
+
+  const handleSetTask = (e) => {
+    setStask(e.target.value);
+  }
+
+  const handleSumit = (e) =>{
+
+    if (e.key === 'Enter') {
+      this.setState({value:e.target.value})
+    }
+  }
+
+  console.log("TASK", task);
+
   return (
     <div className="App">
       <h1>todos</h1>
       <div className="square">
         <form>
           <ul>
-            <input type="text" id='write' placeholder='What needs to be done ?' />
+            <input 
+              type="text" 
+              id='write' 
+              placeholder='What needs to be done ?' 
+              // onChange={handleSetTask} 
+              onKeyPress={handleSumit} />
+             <li><input type="radio" value={task} /></li>
+            {/* {task && <li><input type="radio" value={task} />{task}</li>} */}
+
+            {/* <li><input type="radio" value="list" /></li>
             <li><input type="radio" value="list" /></li>
             <li><input type="radio" value="list" /></li>
             <li><input type="radio" value="list" /></li>
-            <li><input type="radio" value="list" /></li>
-            <li><input type="radio" value="list"/></li>
+            <li><input type="radio" value="list"/></li> */}
           </ul>
         </form>
         <p># item left</p>
