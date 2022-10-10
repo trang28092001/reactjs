f2c5eb60625799a0f0bef639b7124be16adbe271
Trang <trang28092001@gmail.com>
2022-10-10 15:58:23 +0700

    giao dien

--- src/App.js
+++ src/App.js
@@ -3,47 +3,34 @@ import React, { useState } from 'react';
 
 function App() {
   
-  const [task, setStask] = useState();
+  const [task, setTask] = useState();
+  const [listTask, setListTask] = useState([]);
 
   const handleSetTask = (e) => {
-    setStask(e.target.value);
+    setTask(e.target.value);
   }
 
   const handleSumit = (e) =>{
-
+    let list = [...listTask]
     if (e.key === 'Enter') {
-      this.setState({value:e.target.value})
+      list.push(task);
     }
+    setListTask(list);
   }
-
-  console.log("TASK", task);
+  // console.log("TASK", task);
+  // console.log('List task', listTask)
 
   return (
     <div className="App">
       <h1>todos</h1>
       <div className="square">
-        <form>
-          <ul>
-            <input 
-              type="text" 
-              id='write' 
-              placeholder='What needs to be done ?' 
-              // onChange={handleSetTask} 
-              onKeyPress={handleSumit} />
-             <li><input type="radio" value={task} /></li>
-            {/* {task && <li><input type="radio" value={task} />{task}</li>} */}
-
-            {/* <li><input type="radio" value="list" /></li>
-            <li><input type="radio" value="list" /></li>
-            <li><input type="radio" value="list" /></li>
-            <li><input type="radio" value="list" /></li>
-            <li><input type="radio" value="list"/></li> */}
-          </ul>
-        </form>
-        <p># item left</p>
-        <p onClick="all">All</p>
-        <p onClick="active">Active</p>
-        <p></p>
+          <input 
+            type="text" 
+            id='write' 
+            placeholder='What needs to be done?' 
+            onChange={handleSetTask} 
+            onKeyPress={handleSumit} />
+          {listTask.length > 0 && listTask.map((item) => <li><input type="radio" className='button' />{item}</li>)}
       </div>
     </div>
   );
