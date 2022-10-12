003a57240c0a7037859ba1bfce6483bf1ca97125
Nguyen Thi Duyen <duyennt41@viettel.com.vn>
2022-10-12 08:34:34 +0700

    Init object of task has id, taskName and status.

--- src/App.js
+++ src/App.js
@@ -1,57 +1,68 @@
-import './App.css';
-import React, { useState } from 'react';
-import { getAllByAltText } from '@testing-library/react';
+import "./App.css";
+import React, { useState } from "react";
+import { getAllByAltText } from "@testing-library/react";
 
 function App() {
-  
   const [task, setTask] = useState();
   const [listTask, setListTask] = useState([]);
 
   const handleSetTask = (e) => {
-    setTask(e.target.value);
-  }
+    let lastTask = listTask[listTask.length - 1];
+    let value = e.target.value;
+    if (value) {
+      setTask({
+        id: !lastTask ? 1 : lastTask.id + 1,
+        taskName: e.target.value,
+        status: false,
+      });
+    }
+  };
 
-  const handleSumit = (e) =>{
-    let list = [...listTask]
-    if (e.key === 'Enter') {
+  const handleSumit = (e) => {
+    let list = [...listTask];
+    if (task && e.key === "Enter") {
       list.push(task);
     }
     setListTask(list);
-    setTask('')
-  }
-  
+    setTask();
+  };
+
   let fun = ["All", "Active", "Completed"];
 
-  const myList = fun.map((item) =>(<button key = {item} className='show' >{item}</button>));
-  
-  const map = [
-    ...{id: {tag},
-    name: {handleSumit},
-    status: true,}
-  ];
-
-  const mymap = (e) =>{
-    
-  }
+  const myList = fun.map((item) => (
+    <button key={item} className="show">
+      {item}
+    </button>
+  ));
 
   console.log("TASK", task);
-  console.log('List task', listTask)
+  console.log("List task", listTask);
 
   return (
     <div className="App">
       <h1>todos</h1>
       <div className="square">
-          <input 
-            type="text" 
-            id='write' 
-            placeholder='What needs to be done?' 
-            onChange={handleSetTask} 
-            onKeyPress={handleSumit} />
-          {listTask.length > 0 && listTask.map((tag) => (<li key = {tag}><input type="radio" className='button' />{tag}</li>))}
-          {listTask.length > 0 && <p className='end'>
-            <span >{listTask.length} item left</span>
+        <input
+          type="text"
+          id="write"
+          placeholder="What needs to be done?"
+          // value={""}
+          onChange={handleSetTask}
+          onKeyPress={handleSumit}
+        />
+        {listTask.length > 0 &&
+          listTask.map((task) => (
+            <li key={task.id}>
+              <input type="radio" className="button" />
+              {task.taskName}
+            </li>
+          ))}
+        {listTask.length > 0 && (
+          <p className="end">
+            <span>{listTask.length} item left</span>
             <span>{myList}</span>
-          </p>}
+          </p>
+        )}
       </div>
     </div>
   );
