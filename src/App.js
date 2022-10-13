328f932e45575353aabdafc4034d732c9fd6f95b
DuyenNT1212 <nguyenduyen050726@gmai.com>
2022-10-13 18:39:19 +0700

    Fix btn filter.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 function App() {
   const [task, setTask] = useState();
   const [listTask, setListTask] = useState([]);
+  const [listTaskTmp, setListTaskTmp] = useState([]);
+  const [titleBtn, setTitleBtn] = useState('All');
 
   const handleSetTask = (e) => {
     let lastTask = listTask[listTask.length - 1];
@@ -20,49 +22,60 @@ function App() {
 
   const handleSumit = (e) => {
     let list = [...listTask];
-    if (task && e.key === "Enter") {
+    if (task && e.key === "Enter" && !list.includes(task)) {
       list.push(task);
     }
     setListTask(list);
-    // setTask(" ");
-    
   };
 
-   const toggleChange = (id,e) => {
+  const toggleChange = (id, e) => {
     let temp = [...listTask];
-      for(var i = 0; i < temp.length; i++){
-        if (temp[i].id === id){
-          temp[i].status = !temp[i].status;
-        }
+    for (var i = 0; i < temp.length; i++) {
+      if (temp[i].id === id) {
+        temp[i].status = !temp[i].status;
       }
+    }
     setListTask(temp);
+    if (titleBtn === 'All') {
+      setListTaskTmp(temp)
+    }
   }
+
+  useEffect(() => {
+    if (titleBtn === 'All') {
+      setListTaskTmp(listTask)
+    }
+  }, [listTask, titleBtn])
 
   let fun = ["All", "Active", "Completed"];
-  const handleClickBtn = (item, e) =>{
-    console.log(item);
-    let listClick = [...listTask];
-    if(item === "Active"){
-      listClick.filter(i => i.status === false)
-              .map(filteredList => <li key = {filteredList.id} >{filteredList.taskName}</li> );
 
-    }else if (item === "Completed") {
-      listClick.filter(i => i.status === true)
-               .map(filteredList => <li key = {filteredList.id} >{filteredList.taskName}</li> );
-    } 
+  const handleClickBtn = (item, e) => {
+    setTitleBtn(item)
+  }
+
+  useEffect(() => {
+    let listClick;
+    if (titleBtn === "Active") {
+      listClick = listTaskTmp.filter(i => i.status === false);
+
+    } else if (titleBtn === "Completed") {
+      listClick = listTaskTmp.filter(i => i.status === true);
+    }
     else {
-      listClick.map(i => (i.status === true && i.status === false));
+      listClick = listTaskTmp.filter(i => (i.status === true || i.status === false));
     }
     setListTask(listClick);
-  }
+  }, [titleBtn])
+
   const myList = fun.map((item) => (
-    <button key={item} className="show" onClick={(e) => handleClickBtn(item,e)}>
+    <button key={item} className="show" onClick={(e) => handleClickBtn(item, e)}>
       {item}
     </button>
   ));
 
   console.log("TASK", task);
   console.log("List task", listTask);
+  console.log("List task tmp", listTask);
 
 
   return (
@@ -73,23 +86,23 @@ function App() {
           type="text"
           id="write"
           placeholder="What needs to be done?"
-          // value={""}
+          // value={task.taskName}
           onChange={handleSetTask}
           onKeyPress={handleSumit}
         />
         {listTask.length > 0 &&
           listTask.map((task) => (
             <li key={task.id}>
-               <input type="checkbox" className="button"  name= "checkBtn" checked = {task.status} onChange= {(e) => toggleChange(task.id, e)}/>
+              <input type="checkbox" className="button" name="checkBtn" checked={task.status} onChange={(e) => toggleChange(task.id, e)} />
               <label>{task.taskName}</label>
             </li>
           ))}
-        {listTask.length > 0 && (
+        {/* {listTask.length > 0 && ( */}
           <p className="end">
             <span>{listTask.length} item left</span>
             <span>{myList}</span>
           </p>
-        )}
+        {/* )} */}
       </div>
     </div>
   );
