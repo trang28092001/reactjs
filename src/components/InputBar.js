9eb9bf95719aed6fd53724e5cb1ca3d0092f9622
Nguyen Thi Duyen <duyennt41@viettel.com.vn>
2022-10-15 15:34:22 +0700

    Refactor code.

--- src/components/InputBar.js
+++ src/components/InputBar.js
@@ -1,38 +1,56 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 
 export function InputBar(props) {
-    const [task, setTask] = useState();
-    let listTask = props.listTask;
+  const [task, setTask] = useState();
+  let listTask = props.listTask;
 
-    console.log("TASK", task);
+  console.log("TASK", task);
 
-    const handleSetTask = (e) => {
-        let lastTask = listTask[listTask.length - 1];
-        let value = e.target.value;
-        if (value) {
-          setTask({
-            id: !lastTask ? 1 : lastTask.id + 1,
-            taskName: e.target.value,
-            status: false,
-          });
-        }
-    };
-    
-    const handleSumit = (e) => {
-        let list = [...listTask];
-        if (task && e.key === "Enter" && !list.includes(task)) {
-          list.push(task);
-        }
-        props.setListTask(list);
-    };
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
 
-    return (
-        <input
-            type="text"
-            id="write"
-            placeholder="What needs to be done?"
-            onChange={handleSetTask}
-            onKeyPress={handleSumit}
-        />
-    );
+  const handleSubmit = () => {
+    props.createTodo(task);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && task != "") {
+      handleSubmit();
+      setTask("");
+    }
+  };
+
+  console.log("task", task);
+
+  // const handleSetTask = (e) => {
+  //   let lastTask = listTask[listTask.length - 1];
+  //   let value = e.target.value;
+  //   if (value) {
+  //     setTask({
+  //       id: !lastTask ? 1 : lastTask.id + 1,
+  //       taskName: e.target.value,
+  //       status: false,
+  //     });
+  //   }
+  // };
+
+  // const handleSumit = (e) => {
+  //   let list = [...listTask];
+  //   if (task && e.key === "Enter" && !list.includes(task)) {
+  //     list.push(task);
+  //   }
+  //   props.setListTask(list);
+  // };
+
+  return (
+    <input
+      type="text"
+      id="write"
+      placeholder="What needs to be done?"
+      value={task}
+      onChange={handleChange}
+      onKeyPress={handleKeyDown}
+    />
+  );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,32 +1,38 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 
-export function TodoList(props){
-    let listTask = props.listTask;
-    let titleBtn = props.titleBtn;
+export function TodoList(props) {
+  let listTask = props.listTask;
+  let titleBtn = props.titleBtn;
 
-    const toggleChange = (id, e) => {
-        let temp = [...listTask];
-        for (var i = 0; i < temp.length; i++) {
-          if (temp[i].id === id) {
-            temp[i].status = !temp[i].status;
-          }
-        }
-        props.setListTask(temp);
-        if (titleBtn === 'All') {
-          props.setListTaskTmp(temp)
-        }
+  const toggleChange = (id, e) => {
+    let temp = [...listTask];
+    for (var i = 0; i < temp.length; i++) {
+      if (temp[i].id === id) {
+        temp[i].status = !temp[i].status;
       }
+    }
+    props.setListTask(temp);
+    if (titleBtn === "All") {
+      props.setListTaskTmp(temp);
+    }
+  };
 
-    return(
-        <>
-        {listTask.length > 0 &&
-            listTask.map((task) => (
-            <li key={task.id}>
-                <input type="checkbox" className="button" name="checkBtn" checked={task.status} onChange={(e) => toggleChange(task.id, e)} />
-                <label>{task.taskName}</label>
-            </li>
+  return (
+    <>
+      {listTask.length > 0 &&
+        listTask.map((task) => (
+          <li key={task.id}>
+            <input
+              type="checkbox"
+              className="button"
+              name="checkBtn"
+              checked={task.status}
+              onChange={(e) => toggleChange(task.id, e)}
+            />
+            <label>{task.taskName}</label>
+          </li>
         ))}
-        </>
-    );
+    </>
+  );
 }
 export default TodoList;
