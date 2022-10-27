ac8196e27b3e448f12c530ce7120f82f69bb6eed
DuyenNT1212 <nguyenduyen050726@gmai.com>
2022-10-27 23:23:41 +0700

    Change style when click button.

--- src/components/ButtonFilter.js
+++ src/components/ButtonFilter.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function ButtonFilter(props) {
 
-  let listButtonTitle = ["All", "Active", "Completed"];
+  let listButtonTitle = [
+    {
+      title: "All",
+      isClicked: true
+    },
+    {
+      title: "Active",
+      isClicked: false
+    },
+    {
+      title: "Completed",
+      isClicked: false
+    }
+  ];
+
+  const [listButtonsState, setListButtonState] = useState(listButtonTitle);
 
   const handleClickBtn = (item, e) => {
-    props.setTitleBtn(item);
+    props.setTitleBtn(item.title);
+    let list = [...listButtonsState];
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].title === item.title) {
+        list[i].isClicked = true;
+      } else {
+        list[i].isClicked = false;
+      }
+    }
+    setListButtonState(list)
   };
 
-  const myList = listButtonTitle.map((item) => (
+  const myList = listButtonsState.map((item) => (
     <button
-      disabled={props.numberTask === 0}
-      key={item}
-      className="show"
+      className={item.isClicked ? 'show button-active': 'show'} 
+      key={item.title}
       onClick={(e) => handleClickBtn(item, e)}
     >
-      {item}
+      {item.title}
     </button>
   ));
 
